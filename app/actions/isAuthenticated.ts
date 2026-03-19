e2bddbb2cdc77cb3f29fb93c8b3456cb08097e14
Giorgiocig <giorgiocignitti@gmail.com>
2026-03-19 11:26:27 +0100

    Delete isAuthenticated

--- app/actions/isAuthenticated.ts
+++
@@ -1,7 +0,0 @@
-"use server";
-import { getCurrentUser } from "./getCurrentUser";
-
-export async function isAuthenticated(): Promise<boolean> {
-  const user = await getCurrentUser();
-  return !!user;
-}
